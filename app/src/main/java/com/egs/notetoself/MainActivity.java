9127c61a2ab3a8e161347ebb324ce5d3a105335e
geghetsikd <geghetsikd@gmail.com>
2019-09-20 12:15:25 +0400

    Added menu to app.

--- app/src/main/java/com/egs/notetoself/MainActivity.java
+++ app/src/main/java/com/egs/notetoself/MainActivity.java
@@ -1,13 +1,36 @@
 package com.egs.notetoself;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.X509TrustManagerExtensions;
 import android.os.Bundle;
+import android.os.Trace;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+            DialogNewNote dialogNewNote = new DialogNewNote();
+            dialogNewNote.show(getSupportFragmentManager(), "123");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +46,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton btnAdd = (ImageButton) findViewById(R.id.imageButton);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DialogNewNote dialogNewNote = new DialogNewNote();
-                dialogNewNote.show(getSupportFragmentManager(), "123");
-            }
-        });
     }
     Note mTempNote = new Note();
 
